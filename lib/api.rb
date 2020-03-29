b157f3d0f5197a80640de3ae31efd0d34192ffa4
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-29 16:32:43 +0000

    moved api.rb and characters.rb to lib folder

---
+++ lib/api.rb
@@ -0,0 +1,14 @@
+require 'pry'
+require 'rest-client'
+require 'json'
+require_relative 'characters.rb'
+
+resp = RestClient.get("https://www.breakingbadapi.com/api/characters")
+
+characters_hash = JSON.parse(resp.body, symbolize_names:true)
+characters_array = characters_hash
+
+characters_array.each do |char|
+  Character.new(char[:char_id], char[:name], char[:birthday], char[:occupation], char[:img], char[:status], char[:appearance], char[:nickname], char[:portrayed], char[:url])
+end
+
