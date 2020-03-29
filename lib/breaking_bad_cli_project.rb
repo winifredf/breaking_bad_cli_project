9139f40ba6c881311a879b70a1d2b7ad19df084c
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-29 03:58:06 +0000

    first commit

---
+++ lib/breaking_bad_cli_project.rb
@@ -0,0 +1,6 @@
+require "breaking_bad_cli_project/version"
+
+module BreakingBadCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/breaking_bad_cli_project/version.rb
@@ -0,0 +1,3 @@
+module BreakingBadCliProject
+  VERSION = "0.1.0"
+end
