61c9d1f61a0e2c0796c18693aac47ba8fa265375
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-29 06:26:34 +0000

    coding Characters

---
+++ characters.rb
@@ -0,0 +1,8 @@
+class Character
+  
+  attr_accessor :char_id, :name, :birthday, :occupation, :img, :status, :appearance, :nickname, :portrayed, :url
+  
+  def initialize(char_id, name, birthday, occupation, img, status, appearance, nickname, portrayed, url)
+    
+  end
+end
