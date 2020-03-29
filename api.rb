7b8e8e655f702fbbbecd525b9e59fc94f7557e5a
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-29 06:16:13 +0000

    updated api.rb

--- api.rb
+++ api.rb
@@ -3,4 +3,7 @@ require 'rest-client'
 require 'json'
 
 resp = RestClient.get("https://www.breakingbadapi.com/api/characters")
+
+characters_hash = JSON.parse(resp.body, symbolize_names:true)
+characters_array = characters_hash
 binding.pry
