189220f591338899530fc4f1f265c1a2e339959e
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-29 06:38:08 +0000

    initialize properties

--- characters.rb
+++ characters.rb
@@ -2,7 +2,22 @@ class Character
   
   attr_accessor :char_id, :name, :birthday, :occupation, :img, :status, :appearance, :nickname, :portrayed, :url
   
+  @@all = []
+  
   def initialize(char_id, name, birthday, occupation, img, status, appearance, nickname, portrayed, url)
-    
+    @char_id = char_id
+    @name = name
+    @birthday = birthday
+    @occupation = occupation
+    @img = img
+    @status = status
+    @appearance = appearance
+    @nickname = nickname
+    @portrayed = portrayed
+    @url = url
+    @@all << self
+  
+  def self.all
+    @@all
   end
 end
