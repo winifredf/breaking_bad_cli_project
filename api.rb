de8d11859860a3a94189148d8a25480480cf9950
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-29 07:07:20 +0000

    instantiating new instances

--- api.rb
+++ api.rb
@@ -1,9 +1,14 @@
 require 'pry'
 require 'rest-client'
 require 'json'
+require_relative 'characters'
 
 resp = RestClient.get("https://www.breakingbadapi.com/api/characters")
 
 characters_hash = JSON.parse(resp.body, symbolize_names:true)
 characters_array = characters_hash
-binding.pry
+
+characters_array.each do |char|
+  character.new(char[:char_id], char[:name], char[:birthday], char[:occupation], char[:img], char[:status], char[:appearance], char[:nickname], char[:portrayed], char[:url])
+  
+end
