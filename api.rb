21f1b14ff399b2ea559efb8c0922f5ee4ae82aec
Winifred Frierson <winifred.dykes@hotmail.com>
2020-03-29 15:31:22 +0000

    fixed NameError

--- api.rb
+++ api.rb
@@ -9,6 +9,6 @@ characters_hash = JSON.parse(resp.body, symbolize_names:true)
 characters_array = characters_hash
 
 characters_array.each do |char|
-  character.new(char[:char_id], char[:name], char[:birthday], char[:occupation], char[:img], char[:status], char[:appearance], char[:nickname], char[:portrayed], char[:url])
-  binding.pry
+  Character.new(char[:char_id], char[:name], char[:birthday], char[:occupation], char[:img], char[:status], char[:appearance], char[:nickname], char[:portrayed], char[:url])
 end
+
